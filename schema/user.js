b478a928704a9799166d18b151097a90909c831a
thedevaman <devaman2508@gmail.com>
2026-06-25 09:02:46 +0530

    validation in mongoose

--- schema/user.js
+++ schema/user.js
@@ -1,9 +1,28 @@
 import { model, Schema } from "mongoose";
+import * as EmailValidator from 'email-validator';
 
 const schema = new Schema({
-    fullname: String,
-    email: String,
-    mobile : Number,
+    fullname: {
+        type: String,
+        required : true,
+        trim : true,
+        uppercase : true
+    },
+    email: {
+        type: String,
+        required : true,
+        trim : true,
+        validate:{
+            validator:(value)=>{
+              return EmailValidator.validate(value)
+            },
+            message:"Invalid email"
+        }
+    },
+    mobile : {
+        type: Number,
+        required : true
+    },
 })
 
 export const userSchema = model("User", schema)
